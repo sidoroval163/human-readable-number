b812d1ab8ac7beb6e916798e3e84b786445ecd4a
sidoroval163 <alex.sid.92@mail.ru>
2020-03-18 20:24:57 +0300

    init:complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let numbers = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen"
+    ];
+    let tens = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety"
+    ];
+
+    let result = [];
+    if (!number) result.push(numbers[number]);
+    if (number >= 100) {
+        result.push(numbers[Math.floor(number / 100)]);
+        result.push("hundred");
+        number %= 100;
+    }
+
+    if (number >= 20) {
+        result.push(tens[Math.floor(number / 10) - 2]);
+        number %= 10;
+    }
+
+    if (number >= 0 && !number % 10 == 0) {
+        result.push(numbers[number]);
+    }
+
+    return result.join(" ");
+};
